788c3b4b17b6c2f60b5b0f802c8431e4e8a2bd9f
Keef Aragon <keef.aragon@konscious.net>
2017-06-15 09:50:37 -0700

    couldn't resist doing a little more before ending this session

--- src/meter.hpp
+++ src/meter.hpp
@@ -2,9 +2,62 @@
 #define CXXMETRICS_METER_HPP
 
 #include "ewma.hpp"
+#include "meta.hpp"
 
 namespace cxxmetrics
 {
+
+namespace timeunits
+{
+template<templates::duration_type T>
+struct _milliseconds
+{
+    static constexpr templates::duration_type value = T;
+};
+
+template<templates::duration_type T>
+struct _seconds
+{
+    static constexpr templates::duration_type value = _milliseconds<T>::value * 1000;
+};
+
+template<templates::duration_type T>
+struct _minutes
+{
+    static constexpr templates::duration_type value = _seconds<T>::value * 60;
+};
+
+template<templates::duration_type T>
+struct _hours
+{
+    static constexpr templates::duration_type value = _minutes<T>::value * 60;
+};
+
+template<templates::duration_type T>
+struct _days
+{
+    static constexpr templates::duration_type value = _hours<T>::value * 24;
+};
+
+}
+
+struct rate_period
+{
+    template<unsigned long long T>
+    static constexpr templates::duration_type milliseconds = timeunits::_milliseconds<T>::value;
+
+    template<unsigned long long T>
+    static constexpr templates::duration_type seconds = timeunits::_seconds<T>::value;
+
+    template<unsigned long long T>
+    static constexpr templates::duration_type minutes = timeunits::_minutes<T>::value;
+
+    template<unsigned long long T>
+    static constexpr templates::duration_type hours = timeunits::_hours<T>::value;
+
+    template<unsigned long long T>
+    static constexpr templates::duration_type days = timeunits::_days<T>::value;
+};
 
 template<typename... TDurations>
 class _meter_impl
@@ -18,10 +71,6 @@ class meter : public _meter_impl<TDurations...>
 public:
     meter();
 };
-
-template<>
-class meter<duration::minutes<1>, duration::minutes<5>, duration::minutes<15>> : public _meter_impl<duration::minutes<1>, duration::minutes<5>, duration::minutes<15>>
-{};
 
 }
 
--- test/meter_test.cpp
+++ test/meter_test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <meta.hpp>
+#include <meter.hpp>
 #include <thread>
 #include <ctti/type_id.hpp>
 
@@ -9,7 +9,7 @@ using namespace cxxmetrics;
 
 TEST(meter_test, meter_test_type)
 {
-    using sorted = templates::static_sorter<templates::duration_collection<12, 1, 44, 3, 99, 57, 11, 101, 108, 17, 12, 97, 1>>::type;
+    using sorted = templates::static_sorter<templates::duration_collection<12, 1, 44, 3, 99, 57, 11, 101, rate_period::seconds<15>, 17, 12, 97, 1>>::type;
     using s = templates::static_uniq<sorted>::type;
     cout << ctti::type_id<s>().name().cppstring() << endl;
 }
