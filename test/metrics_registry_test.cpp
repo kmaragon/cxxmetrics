054707862e42c3619fe418eaf2b2b42c6f34c4b4
Keef Aragon <keef.aragon@konscious.net>
2018-08-07 23:59:23 -0700

    A little bit more in the visitor tests

--- test/metrics_registry_test.cpp
+++ test/metrics_registry_test.cpp
@@ -37,18 +37,29 @@ TEST_CASE("Registry visitor visits counters", "[metrics_registry]")
 {
     int names = 0;
     int instances = 0;
+    int total = 0;
 
     metrics_registry<> subject;
-    subject.counter("MyCounter");
-    subject.counter("MyCounter", {{"mytag","tagvalue"}});
+    subject.counter("MyCounter") += 10;
+    subject.counter("MyCounter", {{"mytag","tagvalue"}}) += 45;
 
-    subject.visit_registered_metrics([&names, &instances](const metric_path& path, basic_registered_metric& metric) {
+    subject.visit_registered_metrics([&total, &names, &instances](const metric_path& path, basic_registered_metric& metric) {
         ++names;
-        metric.visit([&instances](const tag_collection& tags, const auto& ctr) {
+        metric.visit([&total, &instances](const tag_collection& tags, const value_snapshot& ctr) {
             ++instances;
+            total += static_cast<int>(ctr.value());
         });
     });
 
     REQUIRE(names == 1);
     REQUIRE(instances == 2);
+    REQUIRE(total == 55);
+
+    subject.visit_registered_metrics([&total](const metric_path& path, basic_registered_metric& metric) {
+        metric.aggregate([&total](const value_snapshot& ctr) {
+            total = ctr.value();
+        });
+    });
+
+    REQUIRE(total == 55);
 }
