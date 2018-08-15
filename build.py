2665476bc27a4501693235bed594d67d2185e7eb
Keef Aragon <keef.aragon@konscious.net>
2018-08-14 23:21:55 -0700

    build_policy=outdated

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(test_folder="test")
+    builder = ConanMultiPackager(test_folder="test",build_policy="outdated")
     builder.add_common_builds()
     builder.run()
